9188aad96f9cbfeb190be781443a53a08fcc34fa
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2023-12-28 20:42:14 +0100

    Fixing issue with particle detection

--- inc/construction.hh
+++ inc/construction.hh
@@ -7,6 +7,11 @@
 #include "G4Box.hh"
 #include "G4PVPlacement.hh"
 #include "G4NistManager.hh"
+#include "G4LogicalSkinSurface.hh"
+#include "G4OpticalSurface.hh"
+#include "G4Element.hh"
+
+#include "detector.hh"
 
 class MyDetectorConstruction : public G4VUserDetectorConstruction
 {
@@ -14,24 +19,35 @@ public:
     MyDetectorConstruction();
     ~MyDetectorConstruction();
 
-    G4LogicalVolume *GetScoringVolume() const { return fScoringVolume; }
-
     virtual G4VPhysicalVolume *Construct();
 
 private:
     G4Box *solidWorld;
-    G4LogicalVolume *logicWorld;
-    G4VPhysicalVolume *physWorld;
-    
-    G4LogicalVolume *fScoringVolume;
+    G4Box *solidScintillator;
+    G4Box *solidDetector;
 
+    G4LogicalVolume *logicWorld;
+    G4LogicalVolume *logicScintillator;
+    G4LogicalVolume *logicDetector;
+
+    G4VPhysicalVolume *physWorld;
+    G4VPhysicalVolume *physScintillator;
+    G4VPhysicalVolume *physDetector;
+    
+    G4OpticalSurface *mirrorSurface;
+    
     G4double xWorld;
     G4double yWorld;
     G4double zWorld;
 
     G4Material *worldMat;
+    G4Material *NaI;
+
+    G4Element *Na;
+    G4Element *I;
     
     void DefineMaterials();
+    virtual void ConstructSDandField();
 };
 
 #endif
--- src/construction.cc
+++ src/construction.cc
@@ -18,6 +18,45 @@ void MyDetectorConstruction::DefineMaterials()
     G4NistManager *nist = G4NistManager::Instance();
 
     worldMat = nist->FindOrBuildMaterial("G4_AIR");
+
+    G4double energy[2] = {1.239841939 * eV / 0.2, 1.239841939 * eV / 0.9};
+
+    G4double reflectivity[2] = {1.0, 1.0};
+
+    G4double rindexNaI[2] = {1.78, 1.78};
+    G4double rindexWorld[2] = {1.0, 1.0};
+
+    G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
+    mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
+    worldMat->SetMaterialPropertiesTable(mptWorld);
+
+    Na = nist->FindOrBuildElement("Na");
+    I = nist->FindOrBuildElement("I");
+    NaI = new G4Material("NaI", 3.67 * g / cm3, 2);
+    NaI->AddElement(Na, 1);
+    NaI->AddElement(I, 1);
+
+    G4double fraction[2] = {1.0, 1.0};
+
+    G4MaterialPropertiesTable *mptNaI = new G4MaterialPropertiesTable();
+    mptNaI->AddProperty("RINDEX", energy, rindexNaI, 2);
+    mptNaI->AddProperty("FASTCOMPONENT", energy, fraction, 2);
+    mptNaI->AddConstProperty("SCINTILLATIONYIELD", 38. / keV);
+    mptNaI->AddConstProperty("RESOLUTIONSCALE", 1.0);
+    mptNaI->AddConstProperty("FASTTIMECONSTANT", 250. * ns);
+    mptNaI->AddConstProperty("YIELDRATIO", 1.);
+    NaI->SetMaterialPropertiesTable(mptNaI);
+
+    mirrorSurface = new G4OpticalSurface("mirrorSurface");
+
+    mirrorSurface->SetType(dielectric_metal);
+    mirrorSurface->SetFinish(ground);
+    mirrorSurface->SetModel(unified);
+
+    G4MaterialPropertiesTable *mptMirror = new G4MaterialPropertiesTable();
+    mptMirror->AddProperty("REFLECTIVITY", energy, reflectivity, 2);
+
+    mirrorSurface->SetMaterialPropertiesTable(mptMirror);
 }
 
 G4VPhysicalVolume *MyDetectorConstruction::Construct()
@@ -26,5 +65,36 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
     logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
     physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true);
 
+    solidScintillator = new G4Box("solidScintillator", 5 * cm, 5 * cm, 6 * cm);
+    logicScintillator = new G4LogicalVolume(solidScintillator, NaI, "logicalScintillator");
+
+    G4LogicalSkinSurface *skin = new G4LogicalSkinSurface("skin", logicWorld, mirrorSurface);
+
+    solidDetector = new G4Box("solidDetector", 1. * cm, 5. * cm, 6 * cm);
+    logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
+    for (G4int i = 0; i < 6; i++)
+    {
+        for (G4int j = 0; j < 16; j++)
+        {
+            G4Rotate3D rotZ(j * 22.5 * deg, G4ThreeVector(0, 0, 1));
+            G4Translate3D transXScint(G4ThreeVector(5. / tan(22.5 / 2 * deg) * cm + 5. * cm, 0. * cm, -40. * cm + i * 15 * cm));
+            G4Transform3D transformScint = (rotZ) * (transXScint);
+
+            G4Translate3D transXDet(G4ThreeVector(5. / tan(22.5 / 2 * deg) * cm + 5. * cm + 6. * cm, 0. * cm, -40. * cm + i * 15 * cm));
+            G4Transform3D transformDet = (rotZ) * (transXDet);
+
+            physScintillator = new G4PVPlacement(transformScint, logicScintillator, "physScintillator", logicWorld, false, i * 16 + j, true);
+
+            physDetector = new G4PVPlacement(transformDet, logicDetector, "physDetector", logicWorld, false, i * 16 + j, true);
+        }
+    }
+
     return physWorld;
+}
+
+void MyDetectorConstruction::ConstructSDandField()
+{
+    MySensitiveDetector *sensDet = new MySensitiveDetector("SensitiveDetector");
+
+    logicDetector->SetSensitiveDetector(sensDet);
 }
